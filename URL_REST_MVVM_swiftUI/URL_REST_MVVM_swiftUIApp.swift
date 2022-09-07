2b2e95c72ae2932bc6ead6513b8394598e92cb99
Marcelino1128 <daniel.rs@ixtapaluca.tecnm.mx>
2022-09-06 19:09:01 -0500

    Initial Commit

---
+++ URL_REST_MVVM_swiftUI/URL_REST_MVVM_swiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  URL_REST_MVVM_swiftUIApp.swift
+//  URL_REST_MVVM_swiftUI
+//
+//  Created by Daniel on 06/09/22.
+//
+
+import SwiftUI
+
+@main
+struct URL_REST_MVVM_swiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
